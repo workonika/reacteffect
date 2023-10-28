81020c7b540fc9341de8b1b435213f8a1ec4a6ee
Kuzmin Anton <antofer@mail.ru>
2023-10-28 09:03:35 +0300

    Transfer from TS to JS; rename variable stored keys

--- root/index.js
+++ root/index.js
@@ -1,26 +1,24 @@
 import { useEffect, useState, useRef } from 'react';
 
-export const createBrowserStorageHook = (storage: Storage) => {
-    const __keys_stored_to_restore__ = '__REACT_HOOK_EFFECT__LOCAL_STORAGE_KEYS_';
+export const createBrowserStorageHook = (storage) => {
+    const __keys_stored_to_restore__ = '__REACT_HOOK_EFFECT__BROWSER_STORAGE_KEYS_';
     const __default_value__ = Math.round(Math.random() * 1_000_000_000_000);
 
     const __store__ = {
-        getItem: (key: string) => storage.getItem(key),
-        setItem: (key: string, value: any) => storage.setItem(key, value),
+        getItem: (key) => storage.getItem(key),
+        setItem: (key, value) => storage.setItem(key, value),
         clear: () => storage.clear(),
-        removeItem: (key: string) => storage.removeItem(key),
+        removeItem: (key) => storage.removeItem(key),
         getLength: () => storage.length,
     };
 
-    type KeyValue = { key: string, value: any };
-
     return (defaultReturnValue = null) => {
 
-        const [currentKeyValue, setCurrentKeyValue] = useState<KeyValue | number>(__default_value__);
-        const [currentKeyRemove, setCurrentKeyRemove] = useState<string | number>(__default_value__);
-        const [clear, setClear] = useState<boolean | number>(__default_value__);
+        const [currentKeyValue, setCurrentKeyValue] = useState(__default_value__);
+        const [currentKeyRemove, setCurrentKeyRemove] = useState(__default_value__);
+        const [clear, setClear] = useState(__default_value__);
         
-        const __REACT_HOOK_EFFECT__BUFFER__ = useRef<string, any>();
+        const __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__ = useRef();
 
         //  This useEffect delete key and value from localStorage
         useEffect(() => {
@@ -28,11 +26,11 @@ export const createBrowserStorageHook = (storage: Storage) => {
                 const storedKeysArray = __store__.getItem(__keys_stored_to_restore__);
 
                 __store__.removeItem(currentKeyRemove);
-                __REACT_HOOK_EFFECT__BUFFER__.current.delete(currentKeyRemove);
+                __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current.delete(currentKeyRemove);
 
                 if (storedKeysArray) {
                     const parsedKeysArray = JSON.parse(storedKeysArray);
-                    const filteredKeysArray = parsedKeysArray.filter((key: string) => key !== currentKeyRemove);
+                    const filteredKeysArray = parsedKeysArray.filter(key => key !== currentKeyRemove);
                     const stringifiedKeysArray = JSON.stringify(filteredKeysArray);
 
                     __store__.setItem(__keys_stored_to_restore__, stringifiedKeysArray);
@@ -66,14 +64,14 @@ export const createBrowserStorageHook = (storage: Storage) => {
             }
         }, [currentKeyValue]);
 
-        //  This useEffect initialize __REACT_HOOK_EFFECT__BUFFER__
+        //  This useEffect initialize __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__
         useEffect(() => {
             const storedKeysArray = __store__.getItem(__keys_stored_to_restore__);
-            const keysWithNonexistentValues = new Set<string>();
+            const keysWithNonexistentValues = new Set();
 
             if (storedKeysArray) {
                 const parsedKeysArray = JSON.parse(storedKeysArray);
-                const keyValueRecords = parsedKeysArray.map((key: string) => {
+                const keyValueRecords = parsedKeysArray.map(key => {
                     const storedValue = __store__.getItem(key);
                     if (storedValue) {
                         const parsedValue = JSON.parse(storedValue);
@@ -84,17 +82,17 @@ export const createBrowserStorageHook = (storage: Storage) => {
                     return null;
                 });
 
-                const purifiedKeyValueRecords = keyValueRecords.filter((keyValue: KeyValue | null) => keyValue !== null);
-                const bufferedValues = new Map<string, KeyValue>(purifiedKeyValueRecords);
-                __REACT_HOOK_EFFECT__BUFFER__.current = bufferedValues;
+                const purifiedKeyValueRecords = keyValueRecords.filter(keyValue => keyValue !== null);
+                const bufferedValues = new Map(purifiedKeyValueRecords);
+                __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current = bufferedValues;
 
                 if (keysWithNonexistentValues.size) {
-                    const purifiedKeysArray = parsedKeysArray.filter((key: string) => !keysWithNonexistentValues.has(key));
+                    const purifiedKeysArray = parsedKeysArray.filter(key => !keysWithNonexistentValues.has(key));
                     const stringifiedKeysArray = JSON.stringify(purifiedKeysArray);
                     __store__.setItem(__keys_stored_to_restore__, stringifiedKeysArray);
                 }
             } else {
-                __REACT_HOOK_EFFECT__BUFFER__.current = new Map<string, KeyValue>();
+                __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current = new Map();
             }
         }, []);
         
@@ -110,30 +108,30 @@ export const createBrowserStorageHook = (storage: Storage) => {
         //  You don't need use JSON.stringify when calling setItem method, 
         //  and you don't need use JSON.parse when calling getItem method in your code
         return {
-            getItem (key: string) {
-                const value = __REACT_HOOK_EFFECT__BUFFER__.current.get(key);
+            getItem (key) {
+                const value = __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current.get(key);
 
                 return value ?? defaultReturnValue;
             },
-            setItem (key: string, value: any) {
-                __REACT_HOOK_EFFECT__BUFFER__.current.set(key, value);
+            setItem (key, value) {
+                __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current.set(key, value);
                 setCurrentKeyValue({ key, value });
             },
             clear () {
-                __REACT_HOOK_EFFECT__BUFFER__.current.clear();
+                __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current.clear();
                 setClear(true);
             },
-            removeItem (key: string) {
-                __REACT_HOOK_EFFECT__BUFFER__.current.delete(key);
+            removeItem (key) {
+                __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current.delete(key);
                 setCurrentKeyRemove(key);
             },
             getLength (wholeStorage = false) {
 
-                return wholeStorage ? __store__.getLength() : __REACT_HOOK_EFFECT__BUFFER__?.current?.size;
+                return wholeStorage ? __store__.getLength() : __REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__?.current?.size;
             },
             getKeys () {
-                if (__REACT_HOOK_EFFECT__BUFFER__.current) {
-                    return [...__REACT_HOOK_EFFECT__BUFFER__.current.keys()]
+                if (__REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current) {
+                    return [...__REACT_HOOK_EFFECT__BROWSER_STORAGE_BUFFER__.current.keys()]
                 }
 
                 return 0;
